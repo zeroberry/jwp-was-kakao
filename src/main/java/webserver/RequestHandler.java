1083d9a6c0e48f8ffd4798fc1e4d432ee12f136e
zeroberry <98147817+zeroberry@users.noreply.github.com>
2024-04-11 17:47:08 +0900

    refactor(RequestHandler): reader 객체 생성 service method로 이동

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,56 +1,50 @@
 package webserver;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import utils.RequestParser;
 import webserver.entity.RequestEntity;
 import webserver.entity.ResponseEntity;
 
+import java.io.*;
+import java.net.Socket;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final Controller controller;
-    
-    private Socket connection;
-    
-    public RequestHandler(Socket connectionSocket, Controller controller) {
+
+    private final Socket connection;
+
+    public RequestHandler(final Socket connectionSocket, final Controller controller) {
         this.connection = connectionSocket;
         this.controller = controller;
     }
-    
+
     @Override
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-            connection.getPort());
-        
+                connection.getPort());
+
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             handle(in, out);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
-    
+
     private void handle(final InputStream in, final OutputStream out) throws IOException {
-        InputStreamReader ir = new InputStreamReader(in);
-        BufferedReader br = new BufferedReader(ir);
-        DataOutputStream dos = new DataOutputStream(out);
-        
-        ResponseEntity responseData = service(br);
+        final DataOutputStream dos = new DataOutputStream(out);
+
+        final ResponseEntity responseData = service(in);
         dos.writeBytes(responseData.toResponseMessage());
         dos.flush();
     }
-    
-    private ResponseEntity service(BufferedReader br) {
+
+    private ResponseEntity service(final InputStream in) {
         try {
-            RequestEntity request = RequestParser.parse(br);
+            final BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            final RequestEntity request = RequestParser.parse(br);
             return controller.service(request);
         } catch (IllegalArgumentException e) {
             logger.error("IllegalArgumentException : {}", e.getMessage());
