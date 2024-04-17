854a26358f344089aca8071eb86f3fe27758b066
zeroberry <98147817+zeroberry@users.noreply.github.com>
2024-04-17 10:31:05 +0900

    refactor(FileService): 상수 분리

--- src/main/java/service/FileService.java
+++ src/main/java/service/FileService.java
@@ -15,15 +15,15 @@ public class FileService {
     private static final String STATIC_PREFIX = "./static/";
     private static final String TEMPLATES_PREFIX = "/templates";
     private static final String TEMPLATES_SUFFIX = "";
+    private static final ClassPathTemplateLoader TEMPLATE_LOADER = new ClassPathTemplateLoader(TEMPLATES_PREFIX, TEMPLATES_SUFFIX);
+    private static final Handlebars HANDLEBARS = new Handlebars(TEMPLATE_LOADER);
 
     public byte[] serveFile(final String path) throws IOException, URISyntaxException {
         return FileIoUtils.loadFileFromClasspath(STATIC_PREFIX + path);
     }
 
     public byte[] render(final String path, final Map<String, Object> data) throws IOException {
-        final TemplateLoader templateLoader = new ClassPathTemplateLoader(TEMPLATES_PREFIX, TEMPLATES_SUFFIX);
-        final Handlebars handlebars = new Handlebars(templateLoader);
-        final Template template = handlebars.compile(path);
+        final Template template = HANDLEBARS.compile(path);
         return template.apply(data).getBytes();
     }
 }
